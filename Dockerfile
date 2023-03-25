976a89eae896eaa475b0843991f3987ace7dfcba
Martin <martin@bech.io>
2023-03-26 00:06:02 +0100

    nix flake

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM haskell:8
-
-WORKDIR /bin
-RUN curl -L https://github.com/sol/hpack/releases/download/0.34.2/hpack_linux.gz > hpack.gz
-RUN bash -c "gunzip *.gz"
-RUN chmod +x hpack
-
-ADD . /app
-WORKDIR /app
---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Signal Program Runner (sigpr)";
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ flake-utils.lib.system.x86_64-linux ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        haskellPackages = pkgs.haskellPackages;
+        pkgsStatic = pkgs.pkgsStatic;
+        staticHaskellPackages = pkgsStatic.haskellPackages;
+        sigpr =
+          haskellPackages.callCabal2nix "sigpr" ./. { };
+        staticSigpr =
+          staticHaskellPackages.callCabal2nix "sigpr" ./. { };
+      in
+      {
+        packages.default = with pkgs.haskell.lib; justStaticExecutables (enableStaticLibraries staticSigpr);
+        devShells.default = haskellPackages.shellFor {
+          packages = p: [
+            (pkgs.haskell.lib.disableOptimization sigpr)
+          ];
+
+          buildInputs = [
+            # nix tools
+            pkgs.nixpkgs-fmt
+            pkgs.nil
+            # haskell tools
+            haskellPackages.haskell-language-server
+            haskellPackages.cabal-install
+            haskellPackages.hpack
+          ];
+        };
+      }
+    );
+}
